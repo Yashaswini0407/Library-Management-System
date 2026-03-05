e4d625f91a1e63ba319ba36190c316ccf6bc82a9
Yashaswini0407 <yashaswinin78@gmail.com>
2026-03-05 22:47:34 +0530

    Update Loan.java

--- src/model/Loan.java
+++ src/model/Loan.java
@@ -1,1 +1,30 @@
 
+package model;
+
+import java.time.LocalDate;
+
+public class Loan {
+
+    private Book book;
+    private Patron patron;
+    private LocalDate checkoutDate;
+    private LocalDate returnDate;
+
+    public Loan(Book book, Patron patron) {
+        this.book = book;
+        this.patron = patron;
+        this.checkoutDate = LocalDate.now();
+    }
+
+    public void returnBook() {
+        this.returnDate = LocalDate.now();
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public Patron getPatron() {
+        return patron;
+    }
+}
