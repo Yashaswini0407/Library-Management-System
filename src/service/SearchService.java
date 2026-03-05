66d45cc1fe7aebf227a7a1b4b7caa0e96f6aaa76
Yashaswini0407 <yashaswinin78@gmail.com>
2026-03-05 22:49:45 +0530

    Update SearchService.java

--- src/service/SearchService.java
+++ src/service/SearchService.java
@@ -1,1 +1,36 @@
+package service;
 
+import model.Book;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class SearchService {
+
+    public List<Book> searchByTitle(Collection<Book> books, String title) {
+
+        List<Book> result = new ArrayList<>();
+
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                result.add(book);
+            }
+        }
+
+        return result;
+    }
+
+    public List<Book> searchByAuthor(Collection<Book> books, String author) {
+
+        List<Book> result = new ArrayList<>();
+
+        for (Book book : books) {
+            if (book.getAuthor().equalsIgnoreCase(author)) {
+                result.add(book);
+            }
+        }
+
+        return result;
+    }
+}
