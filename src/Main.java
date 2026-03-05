721443be20b0be68b4392671ccf5f5fd6f907dac
Yashaswini0407 <yashaswinin78@gmail.com>
2026-03-05 22:50:26 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,32 @@
+import model.Book;
+import model.Patron;
+import service.LibraryService;
+import service.SearchService;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        LibraryService library = new LibraryService();
+        SearchService searchService = new SearchService();
+
+        Book book1 = new Book("Java Basics", "James", "111", 2020);
+        Book book2 = new Book("Clean Code", "Robert Martin", "222", 2008);
+
+        Patron patron = new Patron("1", "Alice");
+
+        library.addBook(book1);
+        library.addBook(book2);
+
+        library.registerPatron(patron);
+
+        library.checkoutBook("111", "1");
+
+        library.returnBook("111", "1");
+
+        System.out.println("Search Result:");
+
+        searchService.searchByTitle(library.getAllBooks(), "Java Basics")
+                .forEach(book -> System.out.println(book.getTitle()));
+    }
+}
