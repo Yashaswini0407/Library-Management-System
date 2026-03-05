142dac12021fff9d19a647cb6b47ddfc48e9c643
Yashaswini0407 <yashaswinin78@gmail.com>
2026-03-05 22:46:58 +0530

    Update Patron.java

--- src/model/Patron.java
+++ src/model/Patron.java
@@ -1,1 +1,38 @@
 
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patron {
+
+    private String id;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Patron(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+}
